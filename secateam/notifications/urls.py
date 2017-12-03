07943f2dd5360eb46550768767a1ffdb15771823
Dtutt95 <dtutt95@gmail.com>
2017-12-03 12:11:33 -0600

    Needs to be hooked up to back end

--- secateam/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from notifications import views
-
-
-app_name = 'notifications'
-
-urlpatterns = [
-    url(r'^$', views.SubmitNotificationView.as_view(), name='notifications-view'),
-]
